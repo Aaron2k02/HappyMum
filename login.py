778ca947714e7e261a82566b66cd85dd5a49552b
Khira <u2000780@siswa.um.edu.my>
2023-05-06 03:15:24 +0800

    push

--- login.py
+++ login.py
@@ -1,56 +1,34 @@
 import streamlit as st
 
-def login():
-    st.markdown("<h2 style='text-align: center; color: pink;'>Login Page</h2>", unsafe_allow_html=True)
-    username = st.sidebar.text_input("Username")
-    password = st.sidebar.text_input("Password", type="password")
-    if st.sidebar.checkbox("Login"):
-        if username == "example" and password == "12345":
-            st.sidebar.success("Logged in!")
-            return True
-        else:
-            st.sidebar.error("Incorrect username or password")
-    return False
+# Define the username and password
+username = "mummyamalina"
+password = "happymom"
 
-if login():
-    st.title("Main Page")
-    # Add your main page content here
+# Define the Streamlit app
 
-    # Define the pages
-    def home():
-        st.title("Home Page")
-        st.write("Welcome to the home page!")
-    
-    def about():
-        st.title("About Page")
-        st.write("This is the about page.")
-    
-    def contact():
-        st.title("Contact Page")
-        st.write("Please contact us at contact@example.com")
-    
-    # Define the main function to run the app
-    def main():
-        st.set_page_config(page_title="Multi-page App")
-    
-        # Define the navigation menu
-        pages = {
-            "Home": home,
-            "About": about,
-            "Contact": contact
-        }
-    
-        # Render the navigation menu
-        st.sidebar.title("Navigation")
-        selection = st.sidebar.radio("Go to", list(pages.keys()))
-    
-        # Run the selected page
-        page = pages[selection]
-        page()
-
-else:
+def app():
     # Set page config
     st.set_page_config(page_title="happymom.ai", page_icon=":baby:", layout="wide")
 
     # Set up the web app
     st.markdown("<h1 style='text-align: center; color: pink;'>❤ happymom.ai ❤</h1>", unsafe_allow_html=True)
+    st.markdown("<h2 style='text-align: center; color: pink;'>Login Page</h2>", unsafe_allow_html=True)
+
+    # Define the input fields for the user
+    input_username = st.text_input("Username:")
+    input_password = st.text_input("Password:", type="password")
+
+    # Check if the username and password match
+    if input_username == username and input_password == password:
+        st.write("Logged in successfully!")
+        if st.button("Go to Homepage"):
+            st.set_page_config(
+               page_title="Multipage App",
+               page_icon="👋",
+            )
+    elif input_username != "" and input_password != "":
+        st.write("Invalid username or password.")
+
+# Run the Streamlit app
+if __name__ == '__main__':
+    app()
